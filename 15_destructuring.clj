2aacf0c83ef3d46e35f07042a7c0071a10d273c5
tokito <160491609+tokitostr@users.noreply.github.com>
2025-11-10 22:05:32 +0300

    upload 15

---
+++ 15_destructuring.clj
@@ -0,0 +1,50 @@
+(ns koans.15-destructuring
+  (:require [koan-engine.core :refer :all]))
+
+(def test-address
+  {:street-address "123 Test Lane"
+   :city "Testerville"
+   :state "TX"})
+
+(meditations
+  "Destructuring is an arbiter: it breaks up arguments"
+  (= ":bar:foo" ((fn [[a b]] (str b a))
+         [:foo :bar]))
+
+  "Whether in function definitions"
+  (= (str "An Oxford comma list of apples, "
+          "oranges, "
+          "and pears.")
+     ((fn [[a b c]] (str "An Oxford comma list of " a ", " b ", and " c "."))
+      ["apples" "oranges" "pears"]))
+
+  "Or in let expressions"
+  (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
+     (let [[first-name last-name & aliases]
+           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
+       (apply str (interpose " aka " (cons (str first-name " " last-name) aliases)))))
+
+  "You can regain the full argument if you like arguing"
+  (= {:original-parts ["Steven" "Hawking"] :named-parts {:first "Steven" :last "Hawking"}}
+     (let [[first-name last-name :as full-name] ["Steven" "Hawking"]]
+       {
+        :named-parts {:first first-name :last last-name}
+        :original-parts full-name
+       }))
+
+  "Break up maps by key"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{street-address :street-address, city :city, state :state} test-address]
+       (apply str (interpose ", " (list street-address city state)))))
+
+  "Or more succinctly"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{:keys [street-address city state]} test-address]
+       (apply str (interpose ", " (list street-address city state)))))
+
+  "All together now!"
+  (= "Test Testerson, 123 Test Lane, Testerville, TX"
+     ((fn[[a b] {:keys [street-address city state]}] 
+        (apply str (interpose ", " (list (str a " " b) street-address city state)))) 
+      ["Test" "Testerson"] test-address))
+  
